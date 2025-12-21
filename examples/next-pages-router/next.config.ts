086310215833ed25be070d9d391582d54786629f
tilyupo <tilyupo@gmail.com>
2025-12-22 00:26:19 +0300

    chore: cleanup

--- examples/next-pages-router/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@replanejs/next", "@replanejs/react", "@replanejs/sdk"],
-};
-
-export default nextConfig;
