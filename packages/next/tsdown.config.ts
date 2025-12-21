864d565b23efff72d338231f7f3db4b6d4c38321
tilyupo <tilyupo@gmail.com>
2025-12-21 17:31:32 +0300

    chore: ci

--- packages/next/tsdown.config.ts
+++ packages/next/tsdown.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig([
-  {
-    entry: ["./src/index.tsx"],
-    format: ["esm", "cjs"],
-    platform: "neutral",
-    dts: true,
-    external: ["react", "react-dom", "next", "@replanejs/sdk", "@replanejs/react"],
-  },
-  {
-    entry: ["./src/server.tsx"],
-    format: ["esm", "cjs"],
-    platform: "neutral",
-    dts: true,
-    external: ["react", "react-dom", "next", "@replanejs/sdk", "@replanejs/react"],
-  },
-]);
+export default defineConfig({
+  entry: ["./src/index.tsx"],
+  format: ["esm", "cjs"],
+  platform: "neutral",
+  dts: true,
+  external: ["react", "react-dom", "next", "@replanejs/sdk", "@replanejs/react"],
+});
